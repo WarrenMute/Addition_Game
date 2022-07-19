ca503db5163459c4a81b870230524fe0fa653064
WarrenMute <99296450+WarrenMute@users.noreply.github.com>
2022-07-20 02:53:53 +1200

    Add files via upload

--- 04_AG_Base_Component.py
+++ 04_AG_Base_Component.py
@@ -1,12 +1,13 @@
-import numbers
-import random
-import math
-from tkinter import Variable
-from tokenize import Number
-easy = random.randint ( 1, 10) + random.randint (1, 10)
-medium = random.randint(1, 100) + random.randint(1, 100) + random.randint(1, 100)
-hard = random.randint(1, 100) + random.randint(1, 100) + random.randint(1, 100) + random.randint(1, 100)
 
+from ast import Interactive
+import random
+from tkinter import Y, Variable
+from tokenize import Number
+low_num_easy = 0
+high_num_easy = 20
+
+num = random.randint(low_num_easy, high_num_easy)
+num2 = random.randint(low_num_easy, high_num_easy)
 
 # list of valid responses
 yes_no_list = ["yes", "no"]
@@ -38,26 +39,20 @@ def instructions():
     print()
     print("*** To begin with the computer will ask you to choose a number of Questions OR press < enter > for infinite mode *** ")
     print()
-    print("*** Next you will be asked to choose a difficulty *** ")
-    print()
-    print("easy: numbers range from 1-10 and its a 2 number question ")
-    print()
-    print("medium: numbers range from 1-100 and its a 3 number question ")
-    print()
-    print("hard: numbers range from 1-100 and its a 4 number question ")
+    print("*** numbers range from 1-20 and its a 2 number question *** ")
     print()
     print("***  Next you will be asked a math problem that is addition *** ")
     print()
-    print("*** And you will not get a point for every question you get incorrect ***")
+    print("*** And you will try to anwer as many questions correctly as you can ***")
     print()
-    print("*** Your total points will be added at the end of the game ***")
+    print("*** Good Luck! ***")
     print()
     return ""
 
 def yes_no(question):
     valid = False
     while not valid:
-        response = input(question).lower()
+        response = input(question).lower() 
 
         if response == "yes" or response == "y":
             response = "yes"
@@ -68,6 +63,39 @@ def yes_no(question):
             return response
         else:
             print("Please answer yes / no")
+
+def check_rounds():
+ while True:
+     response = input("How many question or <enter> for infinite mode: ")
+     print()
+
+     round_error = "Please type either <enter> " \
+               "or an integer that is more than 0\n"
+
+         # if infinite mode not chosen, check response
+         # Is an integer that is more than 0
+     if response != "":
+         try:
+             response = int(response)
+
+             if response < 1:
+                 print(round_error)
+                 continue
+
+         except ValueError:
+             print(round_error)
+             continue
+
+     return response
+
+
+
+questions_played = 0
+questions_wrong = 0
+
+
+#Answer
+
 
 
 # Welcomes player to the Addition Game
@@ -84,17 +112,67 @@ if played_before == "no":
     
 my_name = input("What is your name? ")
 print()
-difficulty = input("Well, " + my_name +
-                  ". What difficulty would you like ? easy, medium or hard? ")
+ready = choice_checker("Well, " + my_name +
+                  ". Are you ready? ", yes_no_list,  "Please enter yes/no ")
 print()
 
-# Valid responses
-if difficulty == "easy":
-    Variable = easy
-    print("Okay, " + my_name + ". you chose easy mode")
-if difficulty == "medium":
-    Variable = medium
-    print("Okay, " + my_name + ". you chose medium mode")
-if difficulty == "hard":
-    Variable = hard
-    print("Okay, " + my_name + ". you chose hard mode")
+if ready == "no":
+    print("TOO BAD")
+print()
+print()
+ # Ask user for # of rounds, <enter> for infinite mode
+rounds = check_rounds()
+if rounds == "":
+    mode = "infinite"
+    rounds = 5
+else:
+    mode = "regular"
+
+
+end_game = "no"
+while end_game == "no":
+
+    num = random.randint(low_num_easy, high_num_easy)
+    num2 = random.randint(low_num_easy, high_num_easy)
+    # Rounds heading
+    print()
+    if mode == "infinite":
+        heading = "Continuous Mode: " \
+                  "Question{}".format(questions_played)
+        print(heading)
+        rounds += 1
+
+    else:
+        heading = "Question {} of {}".format( questions_played + 1, rounds )
+        print(heading)
+
+    questions_played += 1
+
+    if questions_played -1 == rounds:
+        print()
+        print("GAME IS OVER")
+        print("THANK YOU FOR PLAYING")
+        print("FEEL FREE TO PLAY AGAIN")
+        break
+
+
+
+
+    # Generate questions
+    print()
+    print()
+    easy_question = int(input("What does {} + {} = ? " .format(num, num2)))
+    correct = int(num + num2)
+    if easy_question == correct:
+        print()
+        print("CORRECT")
+
+    else:
+        print()
+        print("INCORRECT")
+        print()
+        print("The correct answer is :", correct)
+
+    if easy_question is not None:
+        error = "Please enter an number"
+
---
+++ 04_AG_questions generation.py
@@ -0,0 +1,17 @@
+import random
+
+def questions():
+
+    question_1 = int(input("What is 12 + 10 "))
+    if question_1 == "22":
+        print("CORRECT")
+    else:
+        print ("INCORRECT")
+
+
+question_1 = int(input("What is 12 + 10 "))
+
+if question_1 == 22:
+    print("CORRECT")
+else:
+    print("INCORRECT")
